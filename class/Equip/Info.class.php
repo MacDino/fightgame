6ed211f531c7d17a4a6362f7ea8ee25e4fa3597d
zhujinbo <zhujb@bsatinfo.com>
2013-08-15 18:16:49 +0800

    bugfix

--- class/Equip/Info.class.php
+++ class/Equip/Info.class.php
@@ -2,11 +2,11 @@
 //获取装备信息
 class Equip_Info
 {	
-	CONST TABLE_USER_EQUID = 'user_equid';
+	CONST TABLE_NAME = 'user_equid';
 	
 	//获取用户的装备信息
 	public static function getEquipListByUserId($userId){
-	        $res = MySql::select(self::TABLE_NAME, array('user_id' => $userId));
+        $res = MySql::select(self::TABLE_NAME, array('user_id' => $userId));
 		return $res;
 	}	
 
