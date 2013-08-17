ad492de95fa87aaa1238d27f02ca87f21a527c85
kaedezyf <76387051@qq.com>
2013-08-17 15:07:25 +0800

    规范格式,增加道具

--- class/Race.class.php
+++ class/Race.class.php
@@ -7,7 +7,11 @@ class Race
     CONST RACE_TSIMSHIAN    = 2;//仙族
     CONST RACE_DEMON        = 3;//魔族
     
-    //获取种族升级属性加成
+    /**
+     * 获取种族升级属性加成
+     * @param int	 $raceId	种族ID	
+     * @return array	
+     */
     public static function getDefaultAttributes($raceId)
     {
 //    	echo "raceId===$raceId";exit;
@@ -20,7 +24,11 @@ class Race
         return FALSE;
     }
     
-    //获取种族升级属性加成
+    /**
+     * 获取种族升级属性加成
+     * @param int	 $raceId	种族ID
+     * @return array
+     */
     public static function getLeveUpAddAttributes($raceId)
     {
         $leveUpAddAttributtesList = Race_Config::levelUpAddAttributesList();
@@ -31,14 +39,20 @@ class Race
         return FALSE;
     }
     
-    //随机一把种族
+    /**
+     * 随机一把种族
+     * @return int
+     */
     public static function randRaceId()
     {
         $raceList = self::getRaceList();
         $raceId = array_rand(array_keys($raceList), 1);
         return $raceId;
     }
-    //获取种族列表
+    /**
+     * 获取种族列表
+     * @return array
+     */
     public static function getRaceList()
     {
         $raceList = array(
@@ -48,7 +62,12 @@ class Race
           );
         return $raceList;
     }
-    //获取对应种族的成长属性
+    /**
+     * 获取对应种族的成长属性
+     * @param int	 $raceId	种族ID
+     * @param array $userAttributes
+     * @return array
+     */
     public static function getGrowUpAttributes($raceId, $userAttributes)
     {
         if(!$raceId || !is_array($userAttributes))return array();
--- webroot/test/user.php
+++ webroot/test/user.php
@@ -1,8 +1,15 @@
+
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>测试用户</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<?php
+echo time();
+?>
 
 <p center>添加用户</p>
 <form method="GET" action="http://kaedezyf.com/webroot/friend/createFriend.php" name="createFriend" target="_blank">
