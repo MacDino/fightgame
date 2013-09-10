b01d7200b442e2b0dd816c42382402abea5d176c
lishengwei123 <lishengwei123@gmail.com>
2013-09-10 23:49:59 +0800

    修复test目录map列表的用户id错误

--- webroot/test/map.php
+++ webroot/test/map.php
@@ -4,14 +4,14 @@ include $_SERVER['DOCUMENT_ROOT'].'/init.inc.php';
 
 echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
 
-$currentUserId = $_REQUEST['user_id'] > 0 ? $_REQUEST['user_id'] : $_COOKIE['login_user_id'];
+$currentUserId = $_REQUEST['user_id'] > 0 ? $_REQUEST['user_id'] : '';
 
 if($currentUserId) {
     $userInfo = User_Info::getUserInfoByUserId($currentUserId);
 
     $interFace = 'map/list';
     $res = Curl::sendRequest($interFace,array());
-   	$mapList = json_decode($res, true);
+    $mapList = json_decode($res, true);
     if(is_array($mapList['d']['map_list']) && count($mapList['d']['map_list'])) {
         echo '<table>';
         foreach ($mapList['d']['map_list'] as $mapId => $map) {
