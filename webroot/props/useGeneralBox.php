32744d687f2570812fcfa7a5ac15ecec6a8c3f82
xugang <xugang@bsatinfo.com>
2013-09-18 23:17:46 +0800

    接口提交

---
+++ webroot/props/useGeneralBox.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * 使用普通类宝箱
+ */
+include $_SERVER['DOCUMENT_ROOT'].'/init.inc.php';
+
+$userId     	= isset($_REQUEST['user_id'])?(int)$_REQUEST['user_id']:'';
+$propsId		= isset($_REQUEST['props_id']) ?(int)$_REQUEST['props_id'] : '';
+
+if(!$userId || !$propsId)
+{
+    $code = 1;
+    $msg = 'user_id and props_id are require';
+    die;
+}
+try {
+    $data = User_Property::useGeneralTreasureBox ($userId, $propsId);
+} catch (Exception $e) {
+	$code = $e->getCode();
+	$msg = $e->getMessage();
+}
