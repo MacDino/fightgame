538c908fc290fb5d9a6e934c998d018e0b2efe73
xugang <xugang@bsatinfo.com>
2013-09-19 00:44:36 +0800

    num默认数为1

--- webroot/props/buyUserProps.php
+++ webroot/props/buyUserProps.php
@@ -1,12 +1,12 @@
 <?php
 /*
- * 购买符咒   适用于属性增强、双倍、挂机、装备打造、装备成长等咒符
+ * 购买符咒   适用于属性增强、双倍、挂机、装备打造、宝箱等
  */
 include $_SERVER['DOCUMENT_ROOT'].'/init.inc.php';
 
 $userId     	= isset($_REQUEST['user_id'])?(int)$_REQUEST['user_id']:'';
 $propsId	    = isset($_REQUEST['props_id'])?(int)$_REQUEST['props_id']:'';
-$num	    	= isset($_REQUEST['num'])?(int)$_REQUEST['num']:0;			
+$num	    	= isset($_REQUEST['num'])?(int)$_REQUEST['num']:1;			
 
 if(!$userId || !$propsId)
 {
