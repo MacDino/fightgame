3f407e4005bfd3e57bd1c656be7151185644ba51
xugang <xugang@bsatinfo.com>
2013-09-25 17:04:23 +0800

    增加是否在使用咒符接口

---
+++ webroot/props/isUseAutoFight.php
@@ -0,0 +1,20 @@
+<?php
+//购买人宠上限
+include $_SERVER['DOCUMENT_ROOT'].'/init.inc.php';
+
+$userId     	= isset($_REQUEST['user_id'])?(int)$_REQUEST['user_id']:'';//用户ID
+
+//基础校验
+if(!$userId)
+{
+    $code = 1;
+    $msg = 'user_id is required';
+    die;
+}
+
+try {
+    $data = User_Property::isuseAutoFight($userId);
+} catch (Exception $e) {
+	$code = $e->getCode();
+	$msg = $e->getMessage();
+}
