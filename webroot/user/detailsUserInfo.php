a716695764d33e0ba648f3c49078c23363bb4ec6
kaedezyf <76387051@qq.com>
2013-09-26 18:28:38 +0800

    重新提交

---
+++ webroot/user/detailsUserInfo.php
@@ -0,0 +1,39 @@
+<?php
+//获取用户信息,用于查看其他人的详情
+include $_SERVER['DOCUMENT_ROOT'].'/init.inc.php';
+
+$userId     = isset($_REQUEST['user_id'])?$_REQUEST['user_id']:'';//用户ID
+//echo $userId;exit;
+if(!$userId)
+{
+    $code = 1;
+    $msg = '传入参数不正确';
+    die;
+}
+
+try {
+	//基本属性
+	$data['base_info'] = User_Info::getUserInfoByUserId($userId);
+    //使用中装备
+    $equip = Equip_Info::getEquipListByUserId($userId, 1);
+    foreach ($equip as $i=>$key){
+    	$equip[$i]['attribute_list'] = json_decode($key['attribute_list'], true);
+    	$equip[$i]['attribute_base_list'] = json_decode($key['attribute_base_list'], true);
+    }
+    $data['equip_info'] = $equip;
+	//角色基本属性(点)
+	$data['baseAttribute'] = User_Info::getUserInfoFightAttribute($userId);
+	//角色成长属性(值)
+	$data['valueAttribute'] = User_Info::getUserInfoFightAttribute($userId, TRUE);
+	//技能属性
+	$data['skill_info'] = Skill_Info::getSkillList($userId, TRUE);
+//	print_r($data);
+    $code = 0;
+    $msg = 'ok';
+//    die;
+} catch (Exception $e) {
+    $code = 1;
+    $msg = '99';
+    die;    
+}
+
