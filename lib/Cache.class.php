d8d0699f9e34f92f8dee198fc453612258a51210
haoxiaokai <macdino@gmail.com>
2013-09-27 18:36:50 +0800

    add cache class

---
+++ lib/Cache.class.php
@@ -0,0 +1,47 @@
+<?php
+
+class Cache
+{
+    private static $_cacheObj = NULL;
+
+    public static function get($key)
+    {
+        if(DEVELOPER)
+        {
+            return apc_get($key); 
+        }else{
+            $obj = self::_getCacheObj();
+            if($obj)
+            {
+                $obj->get($key)
+            }
+        }
+        return FALSE; 
+    }
+    public static function set($key, $value, $ttl = 60)
+    {
+        if(DEVELOPER)
+        {
+            return apc_add($key, $value, self::$_ttl); 
+        }else{
+            $obj = self::_getCacheObj();
+            if($obj)
+            {
+                $obj->set($key, $value, $ttl)
+            }
+        }
+        return FALSE;
+    }
+
+    private static function _getCacheObj()
+    {
+        if(!is_object(self::$_cacheObj))
+        {
+          if(!DEVELOPER)
+          {
+              self::$_cacheObj = memcache_init();
+          }
+        }
+        return self::$_cacheObj;
+    }
+}
