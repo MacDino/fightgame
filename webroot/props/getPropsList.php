61c691a853c28416c2e1d42b30a84845f16f2de2
xugang <xugang@bsatinfo.com>
2013-10-14 15:02:48 +0800

    修改道具接口

--- webroot/props/getPropsList.php
+++ webroot/props/getPropsList.php
@@ -3,9 +3,8 @@
  * 根据分类获取下属的道具列表
  */
 include $_SERVER['DOCUMENT_ROOT'].'/init.inc.php';
-$cateId = $_REQUEST['cate_id'] ? $_REQUEST['cate_id'] : 1;
 try {
-	$data = Props_Info::getPropsListByCateId($cateId);
+	$data = Props_Info::getPropsList();
 } catch (Exception $e) {
 	$code   = 1;
 }
