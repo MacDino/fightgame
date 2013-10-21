f2abd5cd7536ee7d61ea190f8c45cc9e17f42290
lishengwei <lisw@bsatinfo.com>
2013-10-21 12:08:45 +0800

    去除对这个页面的错误显示设置

--- webroot/fight/monster.php
+++ webroot/fight/monster.php
@@ -5,7 +5,6 @@
  * 战斗结果的数据体结构需要进行重构
  * **/
 include $_SERVER['DOCUMENT_ROOT'].'/init.inc.php';
-error_reporting(E_ALL || ~E_NOTICE); //显示除去 E_NOTICE 之外的所有错误信息
 
 $userId             = isset($_REQUEST['user_id']) ? $_REQUEST['user_id'] : 0;
 $mapId              = isset($_REQUEST['map_id']) ? $_REQUEST['map_id'] : 0;
