ec00d78edd911a942ff59870c126f9b829bfa842
xugang <xugang@bsatinfo.com>
2013-10-21 14:05:35 +0800

    返回道具数量

--- webroot/props/getPropsListByCateId.php
+++ webroot/props/getPropsListByCateId.php
@@ -4,8 +4,15 @@
  */
 include $_SERVER['DOCUMENT_ROOT'].'/init.inc.php';
 $cateId = $_REQUEST['cate_id'] ? $_REQUEST['cate_id'] : 0;
+$userId = $_REQUEST['user_id'] ? $_REQUEST['user_id'] : 0;
 try {
-	$data = Props_Info::getPropsListByCateId($cateId);
+	$res = Props_Info::getPropsListByCateId($cateId);
+	empty($res) && $res = array();
+	foreach ($res as $k => $v) {
+		$userPropsNum = User_Property::getPropertyNum($userId, $v['props_id']);
+		$res[$k]['props_num'] = $userPropsNum > 0 ? $userPropsNum : 0;	
+	}	
+	$data = $res;
 } catch (Exception $e) {
 	$code   = 1;
 }
