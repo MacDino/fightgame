345568e65ae2963d947264e693b66e86ffeac52d
lishengwei <lisw@bsatinfo.com>
2013-11-07 13:38:07 +0800

    增加技能释放概率的计算

--- class/NewFightMember.class.php
+++ class/NewFightMember.class.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 class NewFightMember
 {
 	private $_currentBlood = 0;//当前队员的血量
@@ -7,21 +7,22 @@ class NewFightMember
 	private $_memberAttribute = array();//当前队员的属性
 	private $_memberLevel = 0;//当前队员的级别
 	private $_memberId = 0;//当前队员的级别
-	
+
 	private $_currentSkillEffect = array();//当前队员所受技能的影响效果
 	private $_memberSkill = array();//当前队员所拥有的技能
-	private $_memberRace = NULL;//当前队员的种族
+    private $_memberSkillRate = array(); //当前队员所拥有的技能的释放概率
+    private $_memberRace = NULL;//当前队员的种族
 	private $_memberTeamKey = NULL;//当前队员的队伍标识
-    
-    
+
+
     private $_skillEffectSleepRound = 0;//技能效果-休息回合
     private $_skillEffectAttribute = array();//技能效果-属性加成
     private $_skillEffectNotCanUseSkill = array();//技能效果-无法使用的技能
     private $_skillEffectNotCanAttackSkill = array();//技能效果-无法被攻攻到的技能
-    
+
     /**
      array(
-        'attribute' => 
+        'attribute' =>
             array(
                 array('round' => 1, 'value' => array(ConfigDeifine::USER_ATTRIBUTE_DEFENSE => 0.8, )),
                 array('round' => 1, 'value' => array(ConfigDeifine::USER_ATTRIBUTE_DEFENSE => 0.8, )),
@@ -31,26 +32,26 @@ class NewFightMember
                 array('round' => 1, 'value' => array(1021,1023,124)),
                 array('round' => 1, 'value' => array(1021,1023,124)),
             ),
-        'not_can_attack_skill' => 
+        'not_can_attack_skill' =>
             array(
                 array('round' => 1, 'value')
             ),
      );
-     
+
      not_can_use_skill[] => array('round' => 1, 'value' => array(1202,1203,1204,1205),)
      not_can_use_skill[] => array('round' => 1, 'value' => array(1202,1203,1204,1205),)
-     
+
      attribute[] = array('round' => 'value' => array(ConfigDeifine::USER_ATTRIBUTE_DEFENSE => 0.8, ))
      attribute[] = array('round' => 'value' => array(ConfigDeifine::USER_ATTRIBUTE_DEFENSE => 0.8, ))
-     
+
      not_can_attack_skill[] => array('round' => 1, 'value' => array(1202,1203,1204,1205),)
      not_can_attack_skill[] => array('round' => 1, 'value' => array(1202,1203,1204,1205),)
-     
+
      attack_skill[] = array(skill_id => round, )
-     
-     
+
+
      */
-	
+
 	//初始化
 	public function __construct($memberInfo)
 	{
@@ -63,16 +64,38 @@ class NewFightMember
 		$this->_setAttackSkill($memberInfo['have_skillids']);
 		$this->_setDefineSkill($memberInfo['have_skillids']);
 		$this->_setPassiveSkill($memberInfo['have_skillids']);
+        $this->_setAttackSkillRate($memberInfo['skill_rates']['attack']);
+        $this->_setDefineSkillRate($memberInfo['skill_rates']['define']);
 	}
+
+    //根据技能的释放概率进行技能的选择
+    //如果没有获取到相关的技能的话，返回普通攻击
 	public function getMemberAttackSkill()
 	{
-		return array(1201 => 10);
+		if(is_array($this->_memberSkillRate['attack']) && count($this->_memberSkillRate['attack'])) {
+            $skillId = PerRand::getRandResultKey($this->_memberSkillRate['attack']);
+        }
+        $attackList = $this->_memberSkill['attack'];
+        if(isset($attackList[$skillId])) {
+            $return[$skillId] = $attackList[$skillId];
+        }  else {
+            $return[1201] = $this->_memberLevel;
+        }
+        return $return;
 	}
+
 	public function getMemberDefineSkill()
 	{
-		return array(1211 => 10);
+		if(is_array($this->_memberSkillRate['define']) && count($this->_memberSkillRate['define'])) {
+            $skillId = PerRand::getRandResultKey($this->_memberSkillRate['define']);
+        }
+        $defineList = $this->_memberSkill['define'];
+        if(isset($defineList[$skillId])) {
+            $return[$skillId] = $defineList[$skillId];
+        }
+        return $return;
 	}
-    
+
     public function isMemberSleep()
     {
         if($this->_memberSleepRound > 0)
@@ -81,7 +104,7 @@ class NewFightMember
             return TRUE;
         }
         return FALSE;
-        
+
     }
 	//设置用户可用的攻击技能
 	private function _setAttackSkill($userHaveSkills)
@@ -94,7 +117,7 @@ class NewFightMember
 				$memberHaveSkill[$attackSkillId] = $userHaveSkills[$attackSkillId];
 			}
 		}
-		$this->_memberSkill['attack'] = $memberHaveSkill;	
+		$this->_memberSkill['attack'] = $memberHaveSkill;
 	}
 	//设置用户可用的防御技能
 	private function _setDefineSkill($userHaveSkills)
@@ -122,7 +145,56 @@ class NewFightMember
 		}
 		$this->_memberSkill['passive'] = $memberHaveSkill;
 	}
-	//当前队员是否存活
+
+    //设置用户的攻击技能释放概率，总和不超过1 array(1205 => 0.25,1206 => 0.4 ...)
+    //如果没有，根据基础的释放概率，和可使用的技能数量进行平均
+    private function _setAttackSkillRate($userSkillRates = array()) {
+        if(!(is_array($this->_memberSkill['attack']) && count($this->_memberSkill['attack']))) {
+            return array();
+        }
+        //小于1且存在相关概率传入 的情况，将概率赋给相应技能，否则平均计算基础的技能概率
+        if(is_array($userSkillRates) && count($userSkillRates) && array_sum($userSkillRates) <= 1) {
+            foreach ($this->_memberSkill['attack'] as $skillId => $skillLevel) {
+                if(isset($userSkillRates[$skillId])) {
+                    $this->_memberSkillRate['attack'][$skillId] = $userSkillRates[$skillId];
+                }
+            }
+        } else {
+            $allRate        = Skill_Rate::getAttackRates();
+            $attackSkillNum = count($this->_memberSkill['attack']);
+            $defaultRate    = isset($allRate[$attackSkillNum]) ? number_format(($allRate[$attackSkillNum]/$attackSkillNum), 2, '.',''): 0;
+            foreach ($this->_memberSkill['attack'] as $skillId => $skillLevel) {
+                $this->_memberSkillRate['attack'][$skillId] = $defaultRate;
+            }
+        }
+        return ;
+    }
+
+    //设置用户的防御技能释放概率，总和不超过1 array(1205 => 0.25,1206 => 0.4 ...)
+    //如果没有，根据基础的释放概率，和可使用的技能数量进行平均
+    private function _setDefineSkillRate($userSkillRates = array()) {
+        if(!(is_array($this->_memberSkill['define']) && count($this->_memberSkill['define']))) {
+            return array();
+        }
+        //小于1且存在相关概率传入 的情况，将概率赋给相应技能，否则平均计算基础的技能概率
+        if(is_array($userSkillRates) && count($userSkillRates) && array_sum($userSkillRates) <= 1) {
+            foreach ($this->_memberSkill['define'] as $skillId => $skillLevel) {
+                if(isset($userSkillRates[$skillId])) {
+                    $this->_memberSkillRate['define'][$skillId] = $userSkillRates[$skillId];
+                }
+            }
+        } else {
+            $allRate        = Skill_Rate::getDefineRates();
+            $defineSkillNum = count($this->_memberSkill['define']);
+            $defaultRate    = isset($allRate[$defineSkillNum]) ? number_format(($allRate[$defineSkillNum]/$defineSkillNum), 2, '.','') : 0;
+            foreach ($this->_memberSkill['define'] as $skillId => $skillLevel) {
+                $this->_memberSkillRate['define'][$skillId] = $defaultRate;
+            }
+        }
+        return ;
+    }
+
+    //当前队员是否存活
 	public function isAlive()
 	{
 		return $this->_currentBlood>0?TRUE:FALSE;
@@ -212,5 +284,5 @@ class NewFightMember
     	$attribute = $this->_getMemberAttributes();
     	return isset($attribute[ConfigDefine::USER_ATTRIBUTE_DODGE])?$attribute[ConfigDefine::USER_ATTRIBUTE_PSYCHIC]:0;
     }
-    
+
 }
