a061388aaac47dcca392e21c46bc0919cd963f7c
kaedezyf <76387051@qq.com>
2013-11-12 17:03:50 +0800

    新技能

--- class/NewSkillAttr.class.php
+++ class/NewSkillAttr.class.php
@@ -118,8 +118,17 @@ class NewSkillAttr{
 		);
 	}
 	
-	/** 怨灵缠身 */
+	/** 横扫千军 */
 	private static function _1219($level){
+		$coefficient = pow(2,$level);
+		$hurt  = 2.02 * $level + 0.01 *（ $coefficient + $level）/2;
+		return array(
+			'1107' => $hurt,
+		);
+	}
+	
+	/** 怨灵缠身 */
+	private static function _1220($level){
 		$coefficient = pow(2,$level);
 		$dodge  = 2.01 * $level + 0.02 *（ $coefficient + $level）/2;
 		return array(
@@ -127,9 +136,58 @@ class NewSkillAttr{
 		);
 	}
 	
+	/** 回魂术 */
+	private static function _1221($level){
+		return array();
+	}
 	
+	/** 战无止境 */
+	private static function _1222($level){
+		$coefficient = pow(2,$level);
+		$psychic  = 0.5 * $level + 0.009 *（ $coefficient + $level）/2;
+		return array(
+			'1110' => $psychic,
+		);
+	}
 	
+	/** 玉石俱焚 */
+	private static function _1223($level){
+		$coefficient = pow(2,$level);
+		$defense  = 1 * $level + 0.014 *（ $coefficient + $level）/2;
+		return array(
+			'1112' => $defense,
+		);
+	}
 	
+	/** 体修 */
+	private static function _1224($level, $race){
+		
+	}
+	
+	/** 锻造 */
+	private static function _1225($level){
+		
+	}
+	
+	/** 物攻修 */
+	private static function _1202($level){
+		
+	}
+	
+	/** 法攻修 */
+	private static function _1203($level){
+		
+	}
+	
+	/** 法防修 */
+	private static function _1204($level){
+		
+	}
+	
+	/** 物防修 */
+	private static function _1205($level){
+		
+	}
 	
 	
 }
